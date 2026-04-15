ad3353cf45aa605fe47deb5c0bd5ab6a3440e1c3
NixOSDude <107740546+NixOSDude@users.noreply.github.com>
2026-04-15 14:01:07 -0700

    chore: untrack enterprise files, demos, examples from OSS index

--- demos/sovereign-market/frontend/sovereign-market/src/app/components/star-rating/star-rating.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-star-rating',
-  imports: [],
-  templateUrl: './star-rating.html',
-  styleUrl: './star-rating.scss',
-})
-export class StarRating {}
